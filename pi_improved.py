c8a3093d5b95069ae03b1b290acd27d0d8eddefe
Hamish Nouwens <257276989+TavMan16@users.noreply.github.com>
2026-01-31 09:27:21 +0000

    fixed indentation with pylint:9.44

--- pi_improved.py
+++ pi_improved.py
@@ -2,28 +2,28 @@ from mpi4py import MPI
 
 
 def integrand(x: float) -> float:
-   """Function: 4/(1 + x^2)."""
-   return 4.0 / (1.0 + x * x)
+    """Function: 4/(1 + x^2)."""
+    return 4.0 / (1.0 + x * x)
 
 
 def main() -> None:
-   """Computing the integral in parallel."""
-   comm = MPI.COMM_WORLD
-   rank = comm.Get_rank()
-   nproc = comm.Get_size()
-   n_samples = 10_000_000
-   delta = 1.0 / n_samples
+    """Computing the integral in parallel."""
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    nproc = comm.Get_size()
+    n_samples = 10_000_000
+    delta = 1.0 / n_samples
 
-   local_sum = 0.0
+    local_sum = 0.0
 
-   for i in range(rank, n_samples, nproc):
-       x = (i + 0.5) * delta
-       local_sum += integrand(x) * delta
+    for i in range(rank, n_samples, nproc):
+        x = (i + 0.5) * delta
+        local_sum += integrand(x) * delta
 
-   total = comm.reduce(local_sum, op=MPI.SUM, root=0)
+    total = comm.reduce(local_sum, op=MPI.SUM, root=0)
 
-   if rank == 0:
-       print(f"Estimated pi = {total:.12f}")
+    if rank == 0:
+        print(f"Estimated pi = {total:.12f}")
 
 
 if __name__ == "__main__":
