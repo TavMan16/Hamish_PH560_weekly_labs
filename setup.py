71186df82ffd12082ca7f800993cb94f0eea9c3e
Hamish Nouwens <257276989+TavMan16@users.noreply.github.com>
2026-04-06 13:48:05 +0100

    setup script for cython extensions for 2D ising and xy models

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-# Import the setup function used to build Python extension modules.
 from setuptools import setup
-
-# Import the cythonize helper used to compile the Cython source file.
 from Cython.Build import cythonize
 
-
-# Build the Cython extension module in place.
 setup(
-    # Compile the Metropolis kernel source file.
-    ext_modules=cythonize("metropolis_kernel.pyx")
+    ext_modules=cythonize(
+        [
+            "metropolis_kernel.pyx",
+            "xy_metropolis_kernel.pyx",
+        ]
+    )
 )
